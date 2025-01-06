8148aceb91df44dd542c1c2044bb55caa317096f
Javier Martinez Lizama <jfmarli@gmail.com>
2025-01-06 22:30:10 +0100

    added tests using gtest and updated cmake configuration

--- tests/ditest.cpp
+++ tests/ditest.cpp
@@ -1,3 +1,24 @@
 //
 // Created by jml on 1/6/25.
 //
+
+#include <gtest/gtest.h>
+#include "../src/di.h"
+
+TEST(DependencyContainerTest, RegisterAndResolve) {
+    DependencyContainer container;
+
+    // Register and resolve a std::string
+    container.registerType<std::string>();
+    auto resolveString = container.resolve<std::string>();
+    *resolveString = "Hello, Dependency Injection!";
+
+    EXPECT_EQ(*resolveString, "Hello, Dependency Injection!");
+}
+
+TEST(DependencyContainerTest, UnregisteredTypeThrowsException) {
+    DependencyContainer container;
+
+    // Try to resolve a type that has not been registered
+    EXPECT_THROW(container.resolve<int>(), std::runtime_error);
+}
